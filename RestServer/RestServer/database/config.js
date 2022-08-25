06fbe8b0aedb4ab3d9a5f515cce9ab7f55c54786
ktrillos2 <ktrillos2@gmail.com>
2022-08-25 10:42:25 -0500

    Seccion 9

---
+++ RestServer/RestServer/database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_ATLAS, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log("base de datos online");
+	} catch (error) {
+		console.log(error);
+		throw new Error("error a la hora de incializar el proceso");
+	}
+};
+
+module.exports = {
+	dbConnection,
+};
+
